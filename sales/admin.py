8cc6579c42a8e0670ae0a9bbfaf07039ddab95ee
Muhammad Ayaz <ayazkhattakupwork@gmail.com>
2025-10-22 22:09:06 +0500

    Models and Admin.py complete

--- sales/admin.py
+++ sales/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Sale
 
-# Register your models here.
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    list_display = ['product_name', 'date', 'price_purchased', 'price_sold', 'profit']
+    list_filter = ['date', 'product_name']
+    search_fields = ['product_name']
+    date_hierarchy = 'date'
+    
+    def profit(self, obj):
+        return f"${obj.profit:.2f}"
+    profit.short_description = 'Profit'
+
